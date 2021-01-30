08de100d7e9735cefeebc5d4e9a1e7eeeea6bc6d
JennaTranter <jenna.tranter@gmail.com>
2021-01-30 08:32:54 -0800

    use bcrypt to check a password

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({
     email: username
@@ -73,9 +73,11 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
       }
     }
   });
